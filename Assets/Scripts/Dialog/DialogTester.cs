18b3d1c3ef4256dde2e34dff13d495e8f6a1c2ea
Suliac Blineau <suliac.blineau@orange.fr>
2018-12-01 13:28:08 +0100

    Add basic dialog controller

---
+++ Assets/Scripts/Dialog/DialogTester.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTester : MonoBehaviour {
+
+    [SerializeField] private KeyCode m_input;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(m_input))
+            GameManager.DialogController.NextText();
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private static GameManager m_instance;
+
+    void Awake()
+    {
+        if (m_instance == null)
+            m_instance = this;
+        else if (m_instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    [SerializeField] private DialogController m_dialogController;
+    public static DialogController DialogController { get { return m_instance.m_dialogController; } }
+
+}
