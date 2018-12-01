9f5418144505417ce17f9f256a0a2e41c0e31f7c
Xenation <xenations.111@gmail.com>
2018-12-01 16:16:46 +0100

    Basic CharactersManager

---
+++ Assets/Scripts/CharactersManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace LD43 {
+	public class CharactersManager : Singleton<CharactersManager> {
+
+		public List<CharController> characters;
+		private int activeCharacter = 0;
+
+		private void Start() {
+			characters[activeCharacter].Activate();
+		}
+
+		private void Update() {
+			if (characters.Count > 1) {
+				if (Input.GetButtonDown("NextCharacter")) {
+					NextCharacter();
+				}
+			}
+		}
+
+		public void RemoveCharacter(CharController contr) {
+			int remIndex = characters.IndexOf(contr);
+			characters.Remove(contr);
+			contr.Desactivate();
+			if (remIndex < activeCharacter) {
+				activeCharacter--;
+			}
+			if (characters.Count > 0) {
+				activeCharacter = activeCharacter % characters.Count;
+				characters[activeCharacter].Activate();
+			}
+		}
+
+		public void NextCharacter() {
+			characters[activeCharacter].Desactivate();
+			activeCharacter++;
+			activeCharacter = activeCharacter % characters.Count;
+			characters[activeCharacter].Activate();
+		}
+
+	}
+}
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace LD43 {
+	public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+
+		private static T instance;
+		public static T I {
+			get {
+				if (instance == null) {
+					instance = FindObjectOfType<T>();
+				}
+				return instance;
+			}
+		}
+
+	}
+}
