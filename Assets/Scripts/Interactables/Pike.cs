e7f0ab4874a979d9a41aa8414066cdf494681522
Xenation <xenations.111@gmail.com>
2018-12-01 23:10:42 +0100

    Corrected death on spike position

--- Assets/Scripts/Interactables/Pike.cs
+++ Assets/Scripts/Interactables/Pike.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using LD43;
 
+[RequireComponent(typeof(Collider2D))]
 public class Pike : MonoBehaviour {
+
+	private Collider2D col;
+
+	private void Awake() {
+		col = GetComponent<Collider2D>();
+	}
 
 	private void OnTriggerEnter2D(Collider2D collision) {
 		CharController controller = collision.gameObject.GetComponent<CharController>();
 		if (controller != null) {
-			controller.LayDead();
+			controller.LayDead(transform.position.y + col.bounds.extents.y);
 		}
 	}
 
