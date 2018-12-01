991882dd40897025e30ef32a985fbb05ef58f02a
Suliac Blineau <suliac.blineau@orange.fr>
2018-12-01 23:35:58 +0100

    Improve sound & add fade scene transition

--- Assets/Scripts/CharController.cs
+++ Assets/Scripts/CharController.cs
@@ -15,6 +15,8 @@ namespace LD43 {
 
         public PlayerTypesFlag PlayerType;
 
+        [SerializeField] private AudioSource m_deadSound;
+
 		private float gravity = 1f;
 		private float ascentGravity = 1f;
 		private float coastGravity = 1f;
@@ -155,6 +157,8 @@ namespace LD43 {
 			rb.bodyType = RigidbodyType2D.Static;
 			rb.sharedMaterial = matDead;
 			gameObject.layer = LayerMask.NameToLayer("DeadCharacter");
+            if (m_deadSound)
+                m_deadSound.Play();
 		}
 
 	}
